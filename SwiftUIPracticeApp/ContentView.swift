6f18bbc09c429cbc5b724b61280c2888a1006664
Ryota <mathmyg@gmail.com>
2020-06-16 17:01:01 +0900

    Initial Commit

---
+++ SwiftUIPracticeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPracticeApp
+//
+//  Created by Ryota on 2020/06/16.
+//  Copyright © 2020 Ryota. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
